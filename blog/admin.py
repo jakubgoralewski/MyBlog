e192b18413033f4f2f93c7f80494254f94507915
Jakub Goralewski <kuba.goralewski@gmail.com>
2016-03-17 17:50:42 +0100

    added admin for tags

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Tags
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_published', 'author', 'created_date')
     list_editable = ('is_published',)
     prepopulated_fields = {"slug": ("title",)}
+#admin.site.register(Post, PostAdmin)
 
-admin.site.register(Post, PostAdmin)
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
