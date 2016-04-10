7c6c8018f4beec99955c69504091640b15e77f07
Jakub Goralewski <kuba.goralewski@gmail.com>
2016-04-10 18:38:11 +0200

    added tags in templates of posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from blog.views.post import all_posts_view, post_detail
 from blog.views.tag import all_posts_with_tag
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', all_posts_view, name="all_posts"),
     url(r'^post/(?P<slug>[^/]+)/$', post_detail, name="post_detail"),
-
-    url(r'^tag/(?P<tag>[^/]+)/$', all_posts_with_tag, name="post_detail"),
+    url(r'^tag/(?P<tag>[^/]+)/$', all_posts_with_tag, name="all_posts_with_tag"),
 )
