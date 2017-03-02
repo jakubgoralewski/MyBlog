5b249d9509731205003e44a48f4dbcca581d7224
Jakub Goralewski <kuba.goralewski@gmail.com>
2017-03-02 22:21:32 +0100

    heroku static fix

--- MyBlog/settings/heroku.py
+++ MyBlog/settings/heroku.py
@@ -44,7 +44,7 @@ STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'MyBlog', 'static'),
 )
 
 TEMPLATES = [
